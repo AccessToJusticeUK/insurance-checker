e582e41d7f554a2dcf0e57eaba4022fb36c6a26d
Stephanie Siaw <ssiaw@thoughtworks.com>
2017-11-10 15:21:05 +0000

    MISC - SS - Clean up create react app template to remove react logos etc.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import * as React from 'react';
 import './App.css';
-import { Answer, QuestionPanel } from 'shared-form-app';
 
-const logo = require('./logo.svg');
+import { Answer, QuestionPanel } from 'shared-form-app';
 
 class App extends React.Component {
   render() {
 
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <Answer id="1" text="Some text here" example="" />
-          <QuestionPanel question="a question???" />
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <QuestionPanel question="a question???" />
+        <Answer id="1" text="Some text here" example="" />
       </div>
     );
   }
