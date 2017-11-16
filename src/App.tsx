5e3be75f9dcabaa885dd35d57860b3996a3c9cda
Stephanie Siaw <ssiaw@thoughtworks.com>
2017-11-16 23:19:58 +0000

    [#21] - SS - Demo of how to pull in insurance_providers data in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import './App.css';
 
-import { Answer, QuestionPanel } from 'shared-form-app';
+import { Answer } from 'shared-form-app';
 
 class App extends React.Component {
-  render() {
+  componentDidMount() {
+    fetch('http://localhost:3004/insurance_providers')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log('parsed json', json);
+      }).catch((err) => {
+        console.log('parsing failed', err);
+      });
+  }
 
+  render() {
     return (
       <div className="App">
-        <QuestionPanel question="a question???" />
-        <Answer id="1" text="Some text here" example="" />
+        <Answer id="1" text="An option" example="" />
       </div>
     );
   }
