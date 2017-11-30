eaa60a82ff0c51b32ee03e5901b5e821227e6bbb
Lina Alagrami <alagrami@thoughtworks.com>
2017-11-30 03:51:07 +0000

    Started importing from shared form app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import { PagePreamble, PagePreambleSection } from 'shared-form-app';
 
 import { PopulatedInsuranceDirectory } from './components/PopulatedInsuranceDirectory';
 
@@ -7,7 +8,21 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <PopulatedInsuranceDirectory insuranceType="life" />
+          <div>
+            <PagePreamble>
+              <PagePreambleSection>
+                <p><span className="text-large">You may already have legal insurance that you did not know about!</span></p>
+                <p>This page will walk you through the most likely sources of LEI (Legal Expenses Insurance) that
+                            are often bundled with your existing financial products.</p>
+                <p>Having funds to cover your legal expenses will ensure your problem gets priority and resolved quickly.</p>
+              </PagePreambleSection>
+              <PagePreambleSection>
+                <p>Before you start it may be helpful to have any paperwork regarding your bank accounts,
+                                credit cards and insurance to hand.</p>
+              </PagePreambleSection>
+            </PagePreamble>
+          </div>
+          <PopulatedInsuranceDirectory insuranceType="life" />
       </div>
     );
   }
